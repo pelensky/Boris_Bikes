4e209d045310101671a91b0775b1913866aacd70
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-11 09:30:21 +0000

    fix merge conflict

--- lib/bike.rb
+++ lib/bike.rb
@@ -2,21 +2,8 @@
 class Bike
 <<<<<<< HEAD
 
-def working
-  @working
-end
-
-def working=(working)
-  @working = working
-end
 
 
-  def initialize(working = true)
-    @working = working
-=======
-  def working?
-    true
-    
 >>>>>>> e4641d4c1088ec7406a1f0108a3ad947efd09516
   end
 
