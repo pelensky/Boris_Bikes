67c902d0c46322fb6ae173c02575a9bfbf154d5e
Dan Pelensky <dan@pelensky.com>
2016-11-12 09:39:44 +0000

    all files there

---
+++ spec/bike_spec.rb
@@ -0,0 +1,12 @@
+require "./lib/docking_station.rb"
+require "./lib/bike.rb"
+
+describe Bike do
+  it "Responds to a method called working" do
+  expect(subject).to respond_to(:working)
+  end
+
+
+
+
+end
